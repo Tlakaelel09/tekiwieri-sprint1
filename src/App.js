8f858402de868f9980fa3a4aa22283b8ab7c7ceb
Tlakaelel09 <tlaka.90@gmail.com>
2025-09-04 13:52:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Nav from "./components/Nav";
+//import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Cuestionario from "./components/Cuestionario";
 
