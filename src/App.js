25dd6d9829040c6d2eb731e47dc777620fa871c4
MonRoGa <142746953+MonRoGa@users.noreply.github.com>
2025-09-04 11:46:19 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Cuestionario from "./components/Cuestionario";
+
+// importa el CSS donde metemos nav + hero + cards
+import "./components/nav-hero.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cuestionario" element={<Cuestionario />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
