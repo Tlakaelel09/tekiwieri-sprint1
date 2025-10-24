e4fb449ce828557f786fbb221e6c9c3bde4a89bd
Tlakaelel09 <tlaka.90@gmail.com>
2025-10-23 20:22:12 -0600

    Add especialistas data in JSON format

---
+++ src/data/especialistas.js
@@ -0,0 +1,24 @@
+// src/data/especialistas.json
+[
+  {
+    "nombre": "Cinthia Reyes Tapia",
+    "especialidad": "Suicidología",
+    "ubicacion": "Bahía de Banderas",
+    "modalidad": "Presencial",
+    "contacto": "cinthiareyes@suigeneris-ap.com"
+  },
+  {
+    "nombre": "Marilú Carranco",
+    "especialidad": "Tanatología",
+    "ubicacion": "Bahía de Banderas",
+    "modalidad": "Virtual",
+    "contacto": "maricarranco@suigeneris-ap.com"
+  },
+  {
+    "nombre": "Abraham Aguilar",
+    "especialidad": "Adicciones",
+    "ubicacion": "Puerto Vallarta",
+    "modalidad": "Presencial",
+    "contacto": "abrahamag@suigeneris-ap.com"
+  }
+]
